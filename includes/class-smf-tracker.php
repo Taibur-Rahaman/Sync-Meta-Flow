c43972c917f23dd71434f8a0e130370cd74f92e8
Md Taibur Rahaman <94689383+Taibur-Rahaman@users.noreply.github.com>
2026-09-04 15:14:21 +0600

    feat: make Meta Pixel connection automatic and zero-code

--- includes/class-smf-tracker.php
+++ includes/class-smf-tracker.php
@@ -4,6 +4,7 @@ defined('ABSPATH') || exit;
 class SMF_Tracker {
     public static function init() {
         add_action('wp_enqueue_scripts', array(__CLASS__, 'enqueue'));
+        add_action('wp_head', array(__CLASS__, 'render_meta_pixel'), 1);
         add_action('wp_footer', array(__CLASS__, 'render_script'), 99);
     }
 
@@ -20,7 +21,24 @@ class SMF_Tracker {
             'isCheckout' => function_exists('is_checkout') && is_checkout(),
             'isCart' => function_exists('is_cart') && is_cart(),
             'currency' => function_exists('get_woocommerce_currency') ? get_woocommerce_currency() : '',
+            'metaPixelId' => self::meta_enabled() ? preg_replace('/[^0-9]/', '', (string) get_option('smf_meta_pixel_id', '')) : '',
         ));
+    }
+
+    public static function meta_enabled() {
+        return get_option('smf_meta_enabled', 'no') === 'yes' && trim((string) get_option('smf_meta_pixel_id', '')) !== '';
+    }
+
+    public static function render_meta_pixel() {
+        if (is_admin() || !self::meta_enabled()) return;
+        $pixel = preg_replace('/[^0-9]/', '', (string) get_option('smf_meta_pixel_id', ''));
+        if (!$pixel) return;
+        ?>
+        <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '<?php echo esc_js($pixel); ?>');
+        </script>
+        <?php
     }
 
     public static function render_script() {
@@ -36,31 +54,20 @@ class SMF_Tracker {
     public static function get_or_create_session() {
         $cookie = isset($_COOKIE['smf_session']) ? sanitize_text_field(wp_unslash($_COOKIE['smf_session'])) : '';
         if ($cookie && preg_match('/^[a-f0-9-]{36}$/', $cookie)) return $cookie;
-
         $data = array();
         $allowed = array('fbclid','utm_source','utm_medium','utm_campaign','utm_content','utm_term');
-        foreach ($allowed as $key) {
-            if (isset($_GET[$key])) $data[$key] = sanitize_text_field(wp_unslash($_GET[$key]));
-        }
+        foreach ($allowed as $key) if (isset($_GET[$key])) $data[$key] = sanitize_text_field(wp_unslash($_GET[$key]));
         $key = self::save_session($data);
-        if (!headers_sent()) {
-            setcookie('smf_session', $key, time() + (30 * DAY_IN_SECONDS), COOKIEPATH ?: '/', COOKIE_DOMAIN, is_ssl(), true);
-        }
+        if (!headers_sent()) setcookie('smf_session', $key, time() + (30 * DAY_IN_SECONDS), COOKIEPATH ?: '/', COOKIE_DOMAIN, is_ssl(), true);
         return $key;
     }
 
     public static function save_session($data) {
         global $wpdb;
         $table = $wpdb->prefix . 'smf_tracking_sessions';
-        $key = wp_generate_uuid4();
-        $now = current_time('mysql');
+        $key = wp_generate_uuid4(); $now = current_time('mysql');
         $allowed = array('fbclid','utm_source','utm_medium','utm_campaign','utm_content','utm_term');
-        $row = array(
-            'session_key' => $key,
-            'landing_url' => isset($_SERVER['REQUEST_URI']) ? esc_url_raw(wp_unslash($_SERVER['REQUEST_URI'])) : null,
-            'first_seen' => $now,
-            'last_seen' => $now,
-        );
+        $row = array('session_key' => $key, 'landing_url' => isset($_SERVER['REQUEST_URI']) ? esc_url_raw(wp_unslash($_SERVER['REQUEST_URI'])) : null, 'first_seen' => $now, 'last_seen' => $now);
         foreach ($allowed as $field) $row[$field] = isset($data[$field]) ? sanitize_text_field($data[$field]) : null;
         $wpdb->insert($table, $row);
         return $key;
@@ -69,12 +76,9 @@ class SMF_Tracker {
     public static function touch_session($session_key, $attribution = array()) {
         global $wpdb;
         if (!preg_match('/^[a-f0-9-]{36}$/', $session_key)) return false;
-        $table = $wpdb->prefix . 'smf_tracking_sessions';
-        $updates = array('last_seen' => current_time('mysql'));
+        $table = $wpdb->prefix . 'smf_tracking_sessions'; $updates = array('last_seen' => current_time('mysql'));
         $allowed = array('fbclid','utm_source','utm_medium','utm_campaign','utm_content','utm_term');
-        foreach ($allowed as $field) {
-            if (!empty($attribution[$field])) $updates[$field] = sanitize_text_field($attribution[$field]);
-        }
+        foreach ($allowed as $field) if (!empty($attribution[$field])) $updates[$field] = sanitize_text_field($attribution[$field]);
         return false !== $wpdb->update($table, $updates, array('session_key' => $session_key));
     }
 }
